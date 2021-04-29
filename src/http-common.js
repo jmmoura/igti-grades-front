efbbec79c67ae4170453272df71d4b154e87939b
jmmoura <josielmartinsmoura@gmail.com>
2021-04-29 11:28:05 -0300

    src/http-common.js > baseURL modified

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    baseURL: 'https://grades-api-test.herokuapp.com/',
+    baseURL: 'https://grades-api-test.herokuapp.com',
     headers: {
         'Content-type': 'application/json',
     },
